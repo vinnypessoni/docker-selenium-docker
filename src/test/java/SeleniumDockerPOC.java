b6739113f7fa596718e22117474c5e9be0861760
viniciuspessoni <Ch@conete7>
2023-06-08 15:03:12 +0100

    Refatorando e fazendo mais didatico

--- src/test/java/SeleniumDockerPOC.java
+++ src/test/java/SeleniumDockerPOC.java
@@ -1,40 +1,61 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SeleniumDockerPOC {
 
     WebDriver driver;
 
-    static final String site = "https://viniciuspessoni.com/";
+    //Esse é o endereço do grid do selenium dentro do container docker
     static final String HOST_URL = "http://localhost:4444/wd/hub";
 
+    //Esse é o site que vc quer testar
+    static final String site = "https://viniciuspessoni.com/";
+    ChromeOptions chromeOptions;
 
-    @Test
-    public void assertTitle() throws MalformedURLException {
-
-        ChromeOptions chromeOptions = new ChromeOptions();
+    @BeforeAll
+    public void setUp(){
+        chromeOptions = new ChromeOptions();
         chromeOptions.setCapability("browserVersion", "114.0");
         chromeOptions.setCapability("platformName", "linux");
         // Showing a test name instead of the session id in the Grid UI
         chromeOptions.setCapability("se:name", "Exemplo Tester Global");
+        try {
+            driver = new RemoteWebDriver(new URL("http://localhost:4445"), chromeOptions);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
 
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4445"), chromeOptions);
+    }
 
-        String tituloEsperado = "TESTER GLOBAL";
+
+    @Test
+    public void verificaTitulo(){
+
+        String tituloEsperado = "TESTER GLOBAL\n" +
+                "   por Vinicius Pessoni*\n" +
+                " ";
 
         String tituloEncontrado = "";
 
         driver.get(site);
-        tituloEncontrado = driver.findElement(By.cssSelector(".et_pb_text_inner")).getText();
 
+        tituloEncontrado = driver.findElement(By.cssSelector(".et_pb_text_inner")).getText();
         System.out.println(tituloEncontrado);
+        assertEquals(tituloEsperado, tituloEncontrado);
 
     }
 
